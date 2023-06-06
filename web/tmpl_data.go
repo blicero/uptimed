6bc005091e5dd1568af90b26a9dcb9a9ec013686
Benjamin Walkenhorst <krylon@posteo.de>
2023-06-06 19:46:41 +0200

    Add menu with known hosts to navbar

--- web/tmpl_data.go
+++ web/tmpl_data.go
@@ -2,7 +2,7 @@
 // -*- mode: go; coding: utf-8; -*-
 // Created on 03. 06. 2023 by Benjamin Walkenhorst
 // (c) 2023 Benjamin Walkenhorst
-// Time-stamp: <2023-06-05 20:55:22 krylon>
+// Time-stamp: <2023-06-06 19:42:58 krylon>
 
 package web
 
@@ -18,11 +18,11 @@ type tmplDataBase struct {
 	Timestamp time.Time
 	Debug     bool
 	URL       string
+	Clients   []common.Host
 }
 
 type tmplDataMain struct {
 	tmplDataBase
-	Clients []common.Host
 	Records []common.Record
 }
 
